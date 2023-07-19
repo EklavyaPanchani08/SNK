913f5c1294c28dabe86e4ed895b97ff52f4b4417
Eklavya_Panchani <104212977+EklavyaPanchani08@users.noreply.github.com>
2023-07-19 22:38:07 +0530

    About us page done

---
+++ src/components/BannerForHome.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Button } from "reactstrap";
+import Header from "./Header";
+import "../css/bannerForHome.css";
+
+const BannerForHome = ({ title, subTitter, button }) => {
+  return (
+    <>
+      <div className="banner-for-home-main">
+        <Header />
+        <div className="container banner-for-home">
+        <h1>{title}</h1>
+        <p className="pt-1 pb-3">{subTitter}</p>
+        <Button className="btn btn-black">{button}</Button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default BannerForHome;
