54db5357bbfb148f3e34464209c921e641775348
Eklavya_Panchani <104212977+EklavyaPanchani08@users.noreply.github.com>
2023-07-26 21:54:04 +0530

    Revert "Add vegetables & fruits pages"
    This reverts commit bdbb4ac29efd66c9ecfedaf9b5670c43adf2d626.

--- src/pages/Fruites.jsx
+++ src/pages/Fruites.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Button, Col, Row } from "reactstrap";
 import Banner from "../components/Banner";
-import { FruitsData } from "../data/FruitsData";
+import { product } from "../data/HomeProduct";
 
 const Fruits = () => {
   return (
@@ -9,9 +9,9 @@ const Fruits = () => {
       <Banner title="Fruits" />
       <div className="container my-5">
         <Row>
-          {FruitsData?.map((item) => (
+          {product?.map((item) => (
             <Col md={4} sm={6}>
-              <div className="mx-2 my-4 product" style={{height:'560px'}}>
+              <div className="mx-2 my-4 product">
                 <img src={item?.img} alt={item?.title} />
                 <h5 className="mt-4 mb-3">{item?.title}</h5>
                 <p>{item?.dis}</p>
